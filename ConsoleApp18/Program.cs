06cfc34b1c05ed3d20c50bba861c03e7f4f16352
Шкарпетов Дмитрий <dimashkarpetov2000@gmail.com>
2020-02-14 19:43:26 +0300

    Создание прожекта

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            WriteConsoleText("Привет", 300);
+            Thread.Sleep(1000);
+            WriteConsoleText("Как тебя зовут?", 200);
+            WriteConsoleText($"Привет {Console.ReadLine()}", 200);
+            Console.ReadKey();
+        }
+        public static void WriteConsoleText(string text, int duration)
+        {
+            char[] chartext = text.ToCharArray();
+            for (int i = 0; i < chartext.Length; i++)
+            {
+                Console.Write(chartext[i]);
+                Thread.Sleep(duration);
+            }
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+    }
+}
